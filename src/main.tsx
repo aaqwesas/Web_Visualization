c4a1cc7c76eb04e3a60feaebcaef058e6aed7753
Louis <yankoww1@gmail.com>
2025-02-05 10:02:21 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import React from 'react'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 )
